42288ba07e1173dae87f9e98986880dacebdfac8
muthu-viswanath <146818392+muthu-viswanath@users.noreply.github.com>
2023-10-05 12:28:41 +0530

    Create Guest.java

---
+++ Guest.java
@@ -0,0 +1,14 @@
+//Sample code for Guest Login
+
+
+class Guest{
+
+String guestname;
+String guesttype
+Guest(){}
+Guest( String guestname, String guesttype){
+this.guestname = guestname;
+this.guesttype = guesttype;
+}
+
+}
